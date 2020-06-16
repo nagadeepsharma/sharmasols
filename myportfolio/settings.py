0082919786af7a71af0c7262b5c753e16a6dbf7f
nagadeep <nagadeep.chandragiri@gmail.com>
2020-06-16 20:29:58 +0530

    asd

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r4yl#pe)ev^)1jjvp%joh8&*!h+xtti%pau=qgrq_01qtckk3g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 

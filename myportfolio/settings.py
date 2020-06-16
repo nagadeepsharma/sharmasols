cacf5155eae739ab94d20fb566fb54fbf929d435
nagadeep <nagadeep.chandragiri@gmail.com>
2020-06-16 20:03:58 +0530

    lll

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -125,4 +125,4 @@ STATICFILES_DIRS(os.path.join(BASE_DIR,'static'))
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-django.heroku.settings(locals())
+django_heroku.settings(locals())
